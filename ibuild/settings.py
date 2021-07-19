d5e87409f5c4b561ef327102a17176cd0be98b07
steelthedev <akinwumikaliyanu@gmail.com>
2021-07-19 14:21:23 +0100

    Edited Insecurities

--- ibuild/settings.py
+++ ibuild/settings.py
@@ -41,7 +41,7 @@ INSTALLED_APPS = [
     'app',
     'accounts',
     'dashboard',
-    
+
 ]
 
 MIDDLEWARE = [
@@ -136,7 +136,7 @@ EMAIL_HOST='smtp.gmail.com'
 EMAIL_PORT=587
 EMAIL_USE_TLS=True
 EMAIL_HOST_USER='drlifewritinghub@gmail.com'
-EMAIL_HOST_PASSWORD='kalel2000'
+EMAIL_HOST_PASSWORD=''
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
